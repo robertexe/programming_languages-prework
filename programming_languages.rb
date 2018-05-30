85cf63764c1c8fe754d9da33668c20354beb3175
Robert Tabares <robtabares92@gmail.com>
2018-05-30 21:43:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,14 @@
 def reformat_languages(languages)
   new_hash = {}
-
-  languages.each do | type, details |
-    new_hash[details] = type
+  languages.each do |style, details|
+    details.each do |language, type|
+      if new_hash[language].nil?
+        new_hash[language] = type
+        new_hash[language][:style] = [style]
+      elsif new_hash[language][:style].include?(style) == false
+        new_hash[language][:style] << style
+      end
+    end
   end
-  new_hash
+  return new_hash
 end
